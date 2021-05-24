9ef614e5b2208750598c037998e0c64423b24bc8
best-coder-NA <62673755+best-coder-NA@users.noreply.github.com>
2021-05-24 03:04:45 -0700

    new build

--- js/earn_v2.6db15e0a236eb372e1d0e1708c519461.js
+++ js/earn_v2.6db15e0a236eb372e1d0e1708c519461.js
@@ -203,7 +203,7 @@ async function main() {
     return gaugeContractStake(LINK_PNG_GAUGE, GAUGE_ABI, SPGL_LINK_PNG, SNOWGLOBE_ABI, App);
   }
   const stakeUSDT_PNG = async function() {
-    return gaugeContractStake(WBTC_PNG_GAUGE, GAUGE_ABI, SPGL_WBTC_PNG, SNOWGLOBE_ABI, App);
+    return gaugeContractStake(USDT_PNG_GAUGE, GAUGE_ABI, SPGL_USDT_PNG, SNOWGLOBE_ABI, App);
   }
   const stakeSUSHI_PNG = async function() {
     return gaugeContractStake(SUSHI_PNG_GAUGE, GAUGE_ABI, SPGL_SUSHI_PNG, SNOWGLOBE_ABI, App);
@@ -909,9 +909,6 @@ async function main() {
         `${userSPGL > 1 ? userSPGL.toFixed(3) : userSPGL.toFixed(8)} sPGL`,
         `${ownedPGL > 1 ? ownedPGL.toFixed(3) : ownedPGL.toFixed(8)} PGL - ${pool_percent.toFixed(6)}%`,
         `<div class="col-sm-12 col-md-3 align-items-center text-center snob-tvl pb-10 pb-md-0">
-          <p class="m-0 font-size-12"><ion-icon name="flame-outline"></ion-icon> Your LP value is</p>
-          <p class="m-0 font-size-16 font-weight-regular">${reserve0Owned.toFixed(3)} ${TOKEN_NAMES[token0Address]} / ${reserve1Owned.toFixed(3)} ${TOKEN_NAMES[token1Address]}  </p>
-          <p class="m-0 font-size-12">($${value.toFixed(2)})***</p>
         </div>`,
         totalPoolPGL];
     }).catch( err => {
